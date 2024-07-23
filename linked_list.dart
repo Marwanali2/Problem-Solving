6202e171cb19c0fb95a57c5a1493078af93a1aa0
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-23 19:56:43 +0300

    -implement Linked list p2

--- linked_list.dart
+++ linked_list.dart
@@ -1,4 +1,13 @@
-void main() {}
+void main() {
+  Node head = Node(Task("Task 1", 1));
+  Node node2 = Node(Task("Task 2", 2));
+  Node node3 = Node(Task("Task 3", 3));
+  head.next = node2;
+  node2.next = node3;
+  //  to remove node2 => head.next = node3;
+  print("Task 1 id: ${head.task.id}");
+  print("Task 2 id: ${head.next!.task.text}");
+}
 
 class Task {
   String text;
@@ -8,6 +17,6 @@ class Task {
 
 class Node {
   Task task;
-  Node next;
-  Node(this.task,this.next);
+  Node? next;
+  Node(this.task, [this.next]);
 }
