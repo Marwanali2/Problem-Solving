a7c0af0090b601f98fc00dfd47c01a1ac4a4a584
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-28 04:01:01 +0300

    add item to linked list

--- linked_list.dart
+++ linked_list.dart
@@ -1,12 +1,9 @@
 void main() {
   Node head = Node(Task("Task 1", 1));
-  Node node2 = Node(Task("Task 2", 2));
-  Node node3 = Node(Task("Task 3", 3));
-  head.next = node2;
-  node2.next = node3;
-  //  to remove node2 => head.next = node3;
-  print("Task 1 id: ${head.task.id}");
-  print("Task 2 id: ${head.next!.task.text}");
+  insertTaskToLinkedList(head, Task("Task 2", 2));
+  insertTaskToLinkedList(head, Task("Task 3", 3));
+  //print(head.next!.next!.task.text);
+ 
 }
 
 class Task {
@@ -20,3 +17,13 @@ class Node {
   Node? next;
   Node(this.task, [this.next]);
 }
+
+void insertTaskToLinkedList(Node head, Task newTask) {
+  Node pointer = head;
+  while (pointer.next != null) {
+    pointer = pointer.next!;
+  }
+  pointer.next = Node(newTask);
+}
+
+
