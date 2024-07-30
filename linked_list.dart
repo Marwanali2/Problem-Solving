480a11f90a1769a5cc9e327398ce5be75474d4e9
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-30 20:58:27 +0300

    Display items of linked list

--- linked_list.dart
+++ linked_list.dart
@@ -3,7 +3,8 @@ void main() {
   insertTaskToLinkedList(head, Task("Task 2", 2));
   insertTaskToLinkedList(head, Task("Task 3", 3));
   //print(head.next!.next!.task.text);
- 
+  print('`````````````');
+  printLinkedListText(head);
 }
 
 class Task {
@@ -26,4 +27,16 @@ void insertTaskToLinkedList(Node head, Task newTask) {
   pointer.next = Node(newTask);
 }
 
-
+void printLinkedListText(Node head) {
+  Node?pointer = head;
+  // while (pointer.next != null) {
+  //   print("${pointer.task.text} \n");
+  //   pointer = pointer.next!;
+  // }
+  // print('last node text is ${pointer.task.text}');
+  // حل تاني
+  while (pointer != null) {
+    print(pointer.task.text);
+    pointer = pointer.next;
+  }
+}
